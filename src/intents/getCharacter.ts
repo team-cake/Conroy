2ddb3db93ad04e3b9c2ce2033ce9275c5fd8b04d
team-cake <eka.cheuk@gmail.com>
2020-11-20 21:50:21 +0100

    Set up the files with explanations

---
+++ src/intents/getCharacter.ts
@@ -0,0 +1,9 @@
+/*
+intent: getCharacter
+need: Axios 
+Input human: character name - ${character} as parameter
+get to api https://rickandmortyapi.com/api/episode/${character}
+don't forget to use .catch(err)
+Tried to use different API, didn't work that well for me with the dates. 
+In the end I chose to keep it simple so it will work.
+*/
