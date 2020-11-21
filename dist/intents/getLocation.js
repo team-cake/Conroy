b2f3102f28968c6cdc623cb19804be4dc14373a6
team-cake <eka.cheuk@gmail.com>
2020-11-21 19:46:15 +0100

    First run of dialogflow and server

---
+++ dist/intents/getLocation.js
@@ -0,0 +1,10 @@
+"use strict";
+/*
+intent: getEpisode
+need: Axios
+Input human: location name - ${location} as parameter
+get to api https://rickandmortyapi.com/api/location/?name=${location}
+don't forget to use .catch(err)
+Tried to use different API, didn't work that well for me with the dates.
+In the end I chose to keep it simple so it will work.
+*/
